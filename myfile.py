942c72f4c22ae9341e7517f2caa6741b74e4313b
Varun MK <52526572+VarunMK@users.noreply.github.com>
2024-08-22 18:44:58 +0000

    First Commit

---
+++ myfile.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+for i in range(10):
+    print(":)")
+print("Date right now:",datetime.today())
+a=154
+for i in range(a):
+    a+=a**a
+    a+="add
+    print("Aval:",a)
